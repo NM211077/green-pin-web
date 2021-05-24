6f78365b70a3bf98d7fa8458aa252f33555b5c27
Nataly Moskalenko <y.natali.moskalenko@gmail.com>
2021-05-24 21:09:51 +0300

    Updates

---
+++ precache-manifest.5c58645c06b0083a11adca71a16bea7f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c83f66a35bf87d5eeec434064858d0a",
+    "url": "/green-pin-big-brain/index.html"
+  },
+  {
+    "revision": "74418f56162d2e036934",
+    "url": "/green-pin-big-brain/static/css/main.ddaf0dd1.chunk.css"
+  },
+  {
+    "revision": "1e12bddc151bef75af46",
+    "url": "/green-pin-big-brain/static/js/2.4117ff9b.chunk.js"
+  },
+  {
+    "revision": "2ca8bc6c99819cefed7f6fc66e59f323",
+    "url": "/green-pin-big-brain/static/js/2.4117ff9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74418f56162d2e036934",
+    "url": "/green-pin-big-brain/static/js/main.38c31ba1.chunk.js"
+  },
+  {
+    "revision": "3754ebb32e27bbde60a8",
+    "url": "/green-pin-big-brain/static/js/runtime-main.5dfba704.js"
+  },
+  {
+    "revision": "385799e50019830910d9a8c3e1ad036e",
+    "url": "/green-pin-big-brain/static/media/FrameLogIn.385799e5.png"
+  },
+  {
+    "revision": "4b1426aed04b410a18c05731fd7ec395",
+    "url": "/green-pin-big-brain/static/media/Play.4b1426ae.png"
+  },
+  {
+    "revision": "1c70b2e8c3d59dbd8292ba9705432949",
+    "url": "/green-pin-big-brain/static/media/appStore.1c70b2e8.png"
+  },
+  {
+    "revision": "6d8a377345fe03bc48ef0c8cd111b2a9",
+    "url": "/green-pin-big-brain/static/media/camera.6d8a3773.svg"
+  },
+  {
+    "revision": "0474a59d58cec5a0a12fe0e5034a9bf8",
+    "url": "/green-pin-big-brain/static/media/fontello.0474a59d.woff"
+  },
+  {
+    "revision": "1bb3abb9b1be9a64e05a6a0029fdd5fd",
+    "url": "/green-pin-big-brain/static/media/fontello.1bb3abb9.svg"
+  },
+  {
+    "revision": "c5bb3a97c040178ab056d159d0b433ce",
+    "url": "/green-pin-big-brain/static/media/fontello.c5bb3a97.woff2"
+  },
+  {
+    "revision": "e0a895b6647631861ad60e8dd35692de",
+    "url": "/green-pin-big-brain/static/media/fontello.e0a895b6.eot"
+  },
+  {
+    "revision": "f5857131175155da51595fa26a803728",
+    "url": "/green-pin-big-brain/static/media/fontello.f5857131.ttf"
+  },
+  {
+    "revision": "4943accdffb4dec81a46225124a86256",
+    "url": "/green-pin-big-brain/static/media/logo.4943accd.svg"
+  },
+  {
+    "revision": "b07df44c077dd70d854d78fcf58078c8",
+    "url": "/green-pin-big-brain/static/media/pen.b07df44c.svg"
+  },
+  {
+    "revision": "5c1317f0f7fda841d4592ff374b47a97",
+    "url": "/green-pin-big-brain/static/media/preloader.5c1317f0.gif"
+  }
+]);
